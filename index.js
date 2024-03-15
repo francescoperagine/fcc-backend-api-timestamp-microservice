3efa78e7c142d9f5a7d98b35c9f7b0862352ef25
fperagine <francesco.peragine@apuliasoft.com>
2024-03-15 08:59:26 +0100

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  const { date } = req.params;
 
+  if(!date) {
+    return res.json({
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+    });
+  }
+  
+    if(!isNaN(date) && isFinite(date)) {
+    const ts = parseInt(date);
+    const utcDate = new Date(ts);
+    return res.json({
+      "unix": ts,
+      "utc": utcDate.toUTCString()
+    });
+  }
+
+  const parsedDate = new Date(date);
+  if (parsedDate.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+    
+  return res.json({
+    "unix": parsedDate.getTime(),
+    "utc": parsedDate.toUTCString()
+  });
+  
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
